26b46e3d301c7af6e50aa64622f6b0871108d881
kelvinmburu <mburuhkelvin@gmail.com>
2022-04-11 01:00:15 +0300

    refactor: removed typos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { Quotes } from './quotes.model';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   
   quote: Quotes[];
