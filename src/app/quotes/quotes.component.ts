4229d0dad19926ad948e1147d06ed25d5f567ecf
kelvinmburu <mburuhkelvin@gmail.com>
2022-04-10 22:57:56 +0300

    feat: added vote up and vote down functions

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { Quotes } from '../quotes.model';
 
 @Component({
   selector: 'app-quotes',
@@ -7,7 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
- 
+ @Input() quote!: Quotes
+
+ //Function to use model to for vote up
+
+ voteUp() {
+   this.quote.voteUp();
+ }
+
+ //Function to use model for vote down
+
+ voteDown() {
+   this.quote.voteDown();
+ }
 
   ngOnInit(): void {
   }
