5f75e97790af10756e38f96b6f37d32f7b13af77
kelvinmburu <mburuhkelvin@gmail.com>
2022-04-10 22:02:33 +0300

    refactor: transferred core components

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Quotes } from './quotes.model';
+
 
 @Component({
   selector: 'app-quotes',
@@ -7,15 +7,7 @@ import { Quotes } from './quotes.model';
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
-  quote: Quotes[];
-
-  constructor() {
-    this.quote = [
-      new Quotes('Angular','https://angularjs.org', 3),
-      new Quotes('Mozilla','https://mozilla.org', 2),
-      new Quotes('React Main','https://react.org', 1),
-    ]
-   }
+ 
 
   ngOnInit(): void {
   }
