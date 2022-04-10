4bc59005f9c42f955539e660419a39c0ed37fc68
kelvinmburu <mburuhkelvin@gmail.com>
2022-04-10 22:59:37 +0300

    feat: added a '?' to indicate not mandatory

--- src/app/quotes.model.ts
+++ src/app/quotes.model.ts
@@ -3,7 +3,7 @@ export class Quotes {
     link: string
     votes: number
 
-    constructor(title: string, link: string, votes: number) {
+    constructor(title: string, link: string, votes?: number) {
         this.title = title;
         this.link = link;
         this.votes = votes || 0;
