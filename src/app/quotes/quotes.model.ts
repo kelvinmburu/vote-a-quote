adba23c7c5c96cd4b9a68b22fe220a759a27f4ec
kelvinmburu <mburuhkelvin@gmail.com>
2022-04-10 21:20:36 +0300

    feat: added new model file to export upvote and downvote functions

---
+++ src/app/quotes/quotes.model.ts
@@ -0,0 +1,23 @@
+export class Quotes {
+    title: string;
+    link: string
+    votes: number
+
+    constructor(title: string, link: string, votes: number) {
+        this.title = title;
+        this.link = link;
+        this.votes = votes || 0;
+    }
+
+    //Function to increase the number of votes
+
+    voteUp() {
+        this.votes += 1;
+    }
+
+    //Function to decrease the number of votes
+
+    voteDown() {
+        this.votes -= 1;
+    }
+}
