3f4a53e41a4d376007de99115bc3d87f7a5331b4
kelvinmburu <mburuhkelvin@gmail.com>
2022-04-10 22:03:46 +0300

    test: added files in constructor for testing display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Quotes } from './quotes.model';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   
+  quote: Quotes[];
+
+  constructor() {
+    this.quote = [
+      new Quotes('Angular','https://angularjs.org', 3),
+      new Quotes('Mozilla','https://mozilla.org', 2),
+      new Quotes('React Main','https://react.org', 1),
+    ]
+   }
   addQuote(title: HTMLInputElement, link: HTMLInputElement){
     console.log(title.value)
   }
