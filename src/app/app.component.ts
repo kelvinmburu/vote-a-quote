a12b10f02798fa5cd62ac7a90e119c3985c565fe
kelvinmburu <mburuhkelvin@gmail.com>
2022-04-11 14:09:49 +0300

    feat: added count votes and new name variable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,15 +14,16 @@ export class AppComponent {
 
   constructor() {
     this.quote = [
-      new Quotes('Walt Disney','All our dreams can come true, if we have the courage to pursue them.', 3),
-      new Quotes('Michele Ruiz','If people are doubting how far you can go, go so far that you can’t hear them anymore.', 2),
-      new Quotes('Socrates','Smart people learn from everything and everyone, average people from their experiences, stupid people already have all the answers.', 4),
+      new Quotes('Walt Disney','All our dreams can come true, if we have the courage to pursue them.', 'Kelvin Mburu', 4),
+      new Quotes('Michele Ruiz','If people are doubting how far you can go, go so far that you can’t hear them anymore.', 'Jane Doe', 4),
+      new Quotes('Socrates','Smart people learn from everything and everyone, average people from their experiences, stupid people already have all the answers.', 'John Doe', 3),
     ]
    }
-  addQuote(title: HTMLInputElement, link: HTMLInputElement){
-    this.quote.push(new Quotes(title.value, link.value));
+  addQuote(title: HTMLInputElement, link: HTMLInputElement, name:HTMLInputElement){
+    this.quote.push(new Quotes(title.value, link.value, name.value));
     title.value = '';
     link.value = '';
+    name.value = ''
   }
 
   //Function to sort quotes based on Votes
